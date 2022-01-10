9bcd726d86131a1ea981863b87ced8c0d7757c55
Peter Brown <phbrown@cs.unc.edu>
2022-01-10 19:13:33 +0000

    Added comments citing data sources

--- linegraph.py
+++ linegraph.py
@@ -5,6 +5,7 @@ def main(args:List[str]) -> int:
     # Do nothing, graphically
     win:GraphWin = GraphWin('Click to close', 300, 300)
 
+    # Data from https://scdhec.gov/sites/default/files/afs-covid/county-dashboard/Suppressed County Data by Week.csv
     dates:List[str] = ['11/6', '11/13', '11/20', '11/27', '12/4', '12/11', '12/18']
     cases:List[int] = [224, 211, 243, 211, 312, 313, 343]
     barMaxY = 350
